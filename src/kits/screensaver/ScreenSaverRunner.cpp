luciang/haiku

commit 7fc7a876a4176e3fed8108f8a6701ab5294e57d7

Break up the screen saver update interval into 50 msec ticks. This lets us quickly evaluate if it's time to exit the screen saver, which would previously have blocked until the tick interval was up. This fixes ticket #2339.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@28777 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/screensaver/ScreenSaverRunner.cpp
@@ -173,6 +173,7 @@ ScreenSaverRunner::_CleanUp()
 void
 ScreenSaverRunner::_Run() 
 {
+	const uint32 kTickBase = 50000;
 	if (fWindow->Lock()) {
 		fView->SetViewColor(0, 0, 0);
 		fView->SetLowColor(0, 0, 0);
@@ -183,10 +184,20 @@ ScreenSaverRunner::_Run()
 
 	int32 snoozeCount = 0;
 	int32 frame = 0;
+	int32 tickCounter = 0;
+	bigtime_t tick = fSaver ? fSaver->TickSize() : kTickBase;
 
 	while (!fQuitting) {
-		bigtime_t tick = fSaver ? fSaver->TickSize() : 50000;
-		snooze(tick);
+		// break the idle time up into ticks so that we can evaluate
+		// the quit condition with greater responsiveness
+		// otherwise a screen saver that sets, say, a 30 second tick
+		// will result in the screen saver not responding to deactivation
+		// for that length of time
+		snooze(kTickBase);
+		if (++tickCounter * kTickBase < tick)
+			continue;
+		else
+			tickCounter = 0;
 
 		if (snoozeCount) {
 			// if we are sleeping, do nothing

commit 712b5cc543f6c2562fd8e449783f862c4833605d

Correct slight regression from r28777 - screen saver tick size can be adjusted dynamically, and thus we must update it after each complete tick. Fixes ticket #3211.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@28792 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/screensaver/ScreenSaverRunner.cpp
@@ -19,7 +19,6 @@
 
 #include <stdio.h>
 
-
 ScreenSaverRunner::ScreenSaverRunner(BWindow* window, BView* view,
 		bool preview, ScreenSaverSettings& settings)
 	:
@@ -181,10 +180,10 @@ ScreenSaverRunner::_Run()
 			fHasStarted = fSaver->StartSaver(fView, fPreview) == B_OK;
 		fWindow->Unlock();
 	}
-
+	
 	int32 snoozeCount = 0;
 	int32 frame = 0;
-	int32 tickCounter = 0;
+	bigtime_t lastTickTime = system_time();
 	bigtime_t tick = fSaver ? fSaver->TickSize() : kTickBase;
 
 	while (!fQuitting) {
@@ -194,11 +193,16 @@ ScreenSaverRunner::_Run()
 		// will result in the screen saver not responding to deactivation
 		// for that length of time
 		snooze(kTickBase);
-		if (++tickCounter * kTickBase < tick)
+		if (system_time() - lastTickTime < tick)
 			continue;
-		else
-			tickCounter = 0;
-
+		else { 
+			// re-evaluate the tick time after each successful wakeup - 
+			// screensavers can adjust it on the fly and we must be
+			// prepared to accomodate that
+			tick = fSaver ? fSaver->TickSize() : kTickBase;
+			lastTickTime = system_time();
+		}
+		
 		if (snoozeCount) {
 			// if we are sleeping, do nothing
 			snoozeCount--;