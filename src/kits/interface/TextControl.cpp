luciang/haiku

commit fef237c2f3ebcfc38da81bb8f9b02a281edade8c

* BTextControl is kept navigable to be handled correctly by BControl::SetEnabled(). MakeFocus() transfers anyway the focus on its textview. * BControl::SetEnabled() now calls SetFlags(), BTextControl::SetFlags() is then correctly called. * BControl::SetEnabled() now uses fWantsNav to know if B_NAVIGABLE should be restored
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@29028 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/interface/TextControl.cpp
@@ -11,8 +11,6 @@
 /*!	BTextControl displays text that can act like a control. */
 
 
-#include <stdio.h>
-
 #include <AbstractLayoutItem.h>
 #include <LayoutUtils.h>
 #include <Message.h>
@@ -27,6 +25,7 @@
 
 //#define TRACE_TEXT_CONTROL
 #ifdef TRACE_TEXT_CONTROL
+#	include <stdio.h>
 #	include <FunctionTracer.h>
 	static int32 sFunctionDepth = -1;
 #	define CALLED(x...)	FunctionTracer _ft("BTextControl", __FUNCTION__, \
@@ -512,10 +511,7 @@ BTextControl::SetFlags(uint32 flags)
 		if (flags & B_NAVIGABLE)
 			fText->SetFlags(fText->Flags() | B_NAVIGABLE);
 	}
-
-	// Don't make this one navigable
-	flags &= ~B_NAVIGABLE;
-
+	
 	BView::SetFlags(flags);
 }
 
@@ -929,8 +925,6 @@ BTextControl::_InitData(const char* label, const char* initialText,
 		fDivider = floorf(bounds.Width() / 2.0f);
 
 	uint32 navigableFlags = Flags() & B_NAVIGABLE;
-	if (navigableFlags != 0)
-		BView::SetFlags(Flags() & ~B_NAVIGABLE);
 
 	if (archive)
 		fText = static_cast<BPrivate::_BTextInput_*>(FindView("_input_"));

src/kits/interface/Control.cpp
@@ -344,10 +344,13 @@ BControl::SetEnabled(bool enabled)
 
 	fEnabled = enabled;
 
-	if (fEnabled)
-		BView::SetFlags(Flags() | B_NAVIGABLE);
-	else
-		BView::SetFlags(Flags() & ~B_NAVIGABLE);
+	if (fEnabled && fWantsNav)
+		SetFlags(Flags() | B_NAVIGABLE);
+	else if (!fEnabled && (Flags() & B_NAVIGABLE)) {
+		fWantsNav = true;
+		SetFlags(Flags() & ~B_NAVIGABLE);
+	} else
+		fWantsNav = false;
 
 	if (Window()) {
 		Invalidate(Bounds());
@@ -552,7 +555,7 @@ BControl::InitData(BMessage *data)
 	fEnabled = true;
 	fFocusChanging = false;
 	fTracking = false;
-	fWantsNav = true;
+	fWantsNav = Flags() & B_NAVIGABLE;
 
 	if (data && data->HasString("_fname"))
 		SetFont(be_plain_font, B_FONT_FAMILY_AND_STYLE);

commit 97d5b8e982e752f3d795e3474ecfed175ea4d690

Should fix Shift+TAB on text controls, as it was broken since r29028. Noticed by Karsten Heimrich.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@29134 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/interface/TextControl.cpp
@@ -451,6 +451,10 @@ BTextControl::SetEnabled(bool enabled)
 
 	if (Window()) {
 		fText->MakeEditable(enabled);
+		if (enabled)
+			fText->SetFlags(fText->Flags() | B_NAVIGABLE);
+		else
+			fText->SetFlags(fText->Flags() & ~B_NAVIGABLE);
 
 		_UpdateTextViewColors(enabled);
 
@@ -512,6 +516,9 @@ BTextControl::SetFlags(uint32 flags)
 			fText->SetFlags(fText->Flags() | B_NAVIGABLE);
 	}
 	
+	// Don't make this one navigable
+	flags &= ~B_NAVIGABLE;
+
 	BView::SetFlags(flags);
 }
 
@@ -925,6 +932,8 @@ BTextControl::_InitData(const char* label, const char* initialText,
 		fDivider = floorf(bounds.Width() / 2.0f);
 
 	uint32 navigableFlags = Flags() & B_NAVIGABLE;
+	if (navigableFlags != 0) 	 
+		BView::SetFlags(Flags() & ~B_NAVIGABLE);
 
 	if (archive)
 		fText = static_cast<BPrivate::_BTextInput_*>(FindView("_input_"));