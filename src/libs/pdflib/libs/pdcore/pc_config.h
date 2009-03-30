luciang/haiku

commit fcf619d7809b1233351ecbfe18452f53c48998ac

Preparations for removing __BEOS__ from the compiler defines.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@28305 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/libs/pdflib/libs/pdcore/pc_config.h
@@ -146,7 +146,7 @@
 /* try to identify Mac OS 9 compilers */
 
 #if (defined macintosh || defined __POWERPC__ || defined __CFM68K__) && \
-	!defined MAC && !defined MACOSX && !defined __BEOS__
+	!defined MAC && !defined MACOSX && !(defined(__BEOS__) || !defined(__HAIKU__))
 #define MAC
 #endif
 
@@ -163,7 +163,7 @@
 
 /* ----------------------------------- BeOS --------------------------------- */
 
-#ifdef __BEOS__
+#if defined(__BEOS__) || defined(__HAIKU__)
 #define PDF_PLATFORM	"BeOS"
 
 #ifdef __POWERPC__
@@ -172,7 +172,7 @@
 #define PDC_ISBIGENDIAN 1
 #endif	/* __POWERPC__ */
 
-#endif /* __BEOS__ */
+#endif /* __BEOS__ || __HAIKU__ */
 
 /* --------------------------------- AS/400 --------------------------------- */
 

commit 37661aebc1253f8ac7044af031afa6fb557d0030

Fixed incorrect __HAIKU__ check introduced in r28305.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@29803 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/libs/pdflib/libs/pdcore/pc_config.h
@@ -146,7 +146,7 @@
 /* try to identify Mac OS 9 compilers */
 
 #if (defined macintosh || defined __POWERPC__ || defined __CFM68K__) && \
-	!defined MAC && !defined MACOSX && !(defined(__BEOS__) || !defined(__HAIKU__))
+	!defined MAC && !defined MACOSX && !defined(__BEOS__) && !defined(__HAIKU__)
 #define MAC
 #endif
 