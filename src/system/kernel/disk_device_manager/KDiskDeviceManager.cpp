luciang/haiku

commit 7461b8f74c815ce5f185346c9b25d6eb8ff502b8

* Must be careful which functions to call in the notifications handler, or else   you can easily produce a dead lock. This should fix the system hang   experienced as part of bug #4223.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@32526 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/system/kernel/disk_device_manager/KDiskDeviceManager.cpp
@@ -54,6 +54,16 @@ static const char* kFileSystemPrefix = "file_systems";
 KDiskDeviceManager* KDiskDeviceManager::sDefaultManager = NULL;
 
 
+struct device_event {
+	int32					opcode;
+	const char*				name;
+	dev_t					device;
+	ino_t					directory;
+	ino_t					node;
+	NotificationListener*	listener;
+};
+
+
 struct GetPartitionID {
 	inline partition_id operator()(const KPartition* partition) const
 	{
@@ -119,9 +129,7 @@ private:
 
 class KDiskDeviceManager::DeviceWatcher : public NotificationListener {
 public:
-	DeviceWatcher(KDiskDeviceManager* manager)
-		:
-		fManager(manager)
+	DeviceWatcher()
 	{
 	}
 
@@ -132,45 +140,34 @@ public:
 	virtual void EventOccured(NotificationService& service,
 		const KMessage* event)
 	{
-		int32 opCode = event->GetInt32("opcode", -1);
-		switch (opCode) {
+		int32 opcode = event->GetInt32("opcode", -1);
+		switch (opcode) {
 			case B_ENTRY_CREATED:
 			case B_ENTRY_REMOVED:
 			{
-				const char* name = event->GetString("name", "");
-				dev_t device = event->GetInt32("device", -1);
-				ino_t directory = event->GetInt64("directory", -1);
-				ino_t node = event->GetInt64("node", -1);
-
-				struct stat st;
-				if (vfs_stat_node_ref(device, node, &st) != 0)
+				device_event* deviceEvent = new(std::nothrow) device_event;
+				if (deviceEvent == NULL)
 					break;
 
-				if (S_ISDIR(st.st_mode)) {
-					if (opCode == B_ENTRY_CREATED)
-						add_node_listener(device, node, B_WATCH_DIRECTORY,
-							*this);
-					else
-						remove_node_listener(device, node, *this);
-				} else {
-					if (strcmp(name, "raw") == 0) {
-						// a new raw device was added/removed
-						KPath path(B_PATH_NAME_LENGTH + 1);
-						if (path.InitCheck() != B_OK
-							|| vfs_entry_ref_to_path(device, directory,
-							name, path.LockBuffer(),
-							path.BufferSize()) != B_OK) {
-							break;
-						}
-						path.UnlockBuffer();
+				const char* name = event->GetString("name", NULL);
+				if (name != NULL)
+					deviceEvent->name = strdup(name);
+				else
+					deviceEvent->name = NULL;
 
-						if (opCode == B_ENTRY_CREATED)
-							fManager->CreateDevice(path.Path());
-						else
-							fManager->DeleteDevice(path.Path());
-					}
-				}
+				deviceEvent->opcode = opcode;
+				deviceEvent->device = event->GetInt32("device", -1);
+				deviceEvent->directory = event->GetInt64("directory", -1);
+				deviceEvent->node = event->GetInt64("node", -1);
+				deviceEvent->listener = this;
 
+				// TODO: a real in-kernel DPC mechanism would be preferred...
+				thread_id thread = spawn_kernel_thread(_HandleDeviceEvent,
+					"device event", B_NORMAL_PRIORITY, deviceEvent);
+				if (thread < 0)
+					delete deviceEvent;
+				else
+					resume_thread(thread);
 				break;
 			}
 
@@ -179,8 +176,46 @@ public:
 		}
 	}
 
-private:
-	KDiskDeviceManager* fManager;
+	static status_t _HandleDeviceEvent(void* _event)
+	{
+		device_event* event = (device_event*)_event;
+
+		struct stat st;
+		if (vfs_stat_node_ref(event->device, event->node, &st) != 0) {
+			delete event;
+			return B_ERROR;
+		}
+
+		if (S_ISDIR(st.st_mode)) {
+			if (event->opcode == B_ENTRY_CREATED) {
+				add_node_listener(event->device, event->node, B_WATCH_DIRECTORY,
+					*event->listener);
+			} else {
+				remove_node_listener(event->device, event->node,
+					*event->listener);
+			}
+		} else if (strcmp(event->name, "raw") == 0) {
+			// a new raw device was added/removed
+			KPath path(B_PATH_NAME_LENGTH + 1);
+			if (path.InitCheck() != B_OK
+				|| vfs_entry_ref_to_path(event->device, event->directory,
+						event->name, path.LockBuffer(),
+				path.BufferSize()) != B_OK) {
+				delete event;
+				return B_ERROR;
+			}
+
+			path.UnlockBuffer();
+
+			if (event->opcode == B_ENTRY_CREATED)
+				KDiskDeviceManager::Default()->CreateDevice(path.Path());
+			else
+				KDiskDeviceManager::Default()->DeleteDevice(path.Path());
+		}
+
+		delete event;
+		return B_OK;
+	}
 };
 
 
@@ -211,7 +246,7 @@ KDiskDeviceManager::KDiskDeviceManager()
 	fMediaChecker(-1),
 	fTerminating(false),
 	fDiskSystemWatcher(NULL),
-	fDeviceWatcher(new(nothrow) DeviceWatcher(this)),
+	fDeviceWatcher(new(nothrow) DeviceWatcher()),
 	fNotifications(new(nothrow) DiskNotifications)
 {
 	if (InitCheck() != B_OK)

commit 32ef937e1a86036e32a5b33429d7a90522616402

Fix regression introduced in r32526: registering the device watcher for node monitoring needs to happen synchronously, otherwise a race condition occurs: unless you're very lucky, the event for creating the raw device would happen before the async thread had a change to add the node monitor, and as such the listener would miss it, and never trigger a filesystem/partition scan. As a result, whether or not adding a USB stick would actually show a volume was essentially russian roulette. Fixes ticket #4345.
+alphabranch


git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@32820 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/system/kernel/disk_device_manager/KDiskDeviceManager.cpp
@@ -60,7 +60,6 @@ struct device_event {
 	dev_t					device;
 	ino_t					directory;
 	ino_t					node;
-	NotificationListener*	listener;
 };
 
 
@@ -159,7 +158,29 @@ public:
 				deviceEvent->device = event->GetInt32("device", -1);
 				deviceEvent->directory = event->GetInt64("directory", -1);
 				deviceEvent->node = event->GetInt64("node", -1);
-				deviceEvent->listener = this;
+
+				struct stat st;
+				if (vfs_stat_node_ref(deviceEvent->device, 
+					deviceEvent->node, &st) != 0) {
+					delete deviceEvent;
+					break;
+				}
+				if (S_ISDIR(st.st_mode)) {
+					if (opcode == B_ENTRY_CREATED) {
+						add_node_listener(
+							deviceEvent->device,
+							deviceEvent->node,
+							B_WATCH_DIRECTORY,
+							*this);
+					} else {
+						remove_node_listener(
+							deviceEvent->device,
+							deviceEvent->node,
+							*this);
+					}
+					delete deviceEvent;
+					break;
+				}
 
 				// TODO: a real in-kernel DPC mechanism would be preferred...
 				thread_id thread = spawn_kernel_thread(_HandleDeviceEvent,
@@ -179,22 +200,8 @@ public:
 	static status_t _HandleDeviceEvent(void* _event)
 	{
 		device_event* event = (device_event*)_event;
-
-		struct stat st;
-		if (vfs_stat_node_ref(event->device, event->node, &st) != 0) {
-			delete event;
-			return B_ERROR;
-		}
-
-		if (S_ISDIR(st.st_mode)) {
-			if (event->opcode == B_ENTRY_CREATED) {
-				add_node_listener(event->device, event->node, B_WATCH_DIRECTORY,
-					*event->listener);
-			} else {
-				remove_node_listener(event->device, event->node,
-					*event->listener);
-			}
-		} else if (strcmp(event->name, "raw") == 0) {
+		
+		if (strcmp(event->name, "raw") == 0) {
 			// a new raw device was added/removed
 			KPath path(B_PATH_NAME_LENGTH + 1);
 			if (path.InitCheck() != B_OK
@@ -206,7 +213,6 @@ public:
 			}
 
 			path.UnlockBuffer();
-
 			if (event->opcode == B_ENTRY_CREATED)
 				KDiskDeviceManager::Default()->CreateDevice(path.Path());
 			else