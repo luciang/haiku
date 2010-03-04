luciang/haiku

commit 25115d1100a7ec7765271617dea22a37625a867d

Check for division by zero.
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@35756 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/add-ons/kernel/drivers/power/enhanced_speedstep/frequency.cpp
@@ -144,13 +144,16 @@ est_msr_info(uint64 msr, freq_info** _frequencyInfos)
 
 	int32 freq = info.cpu_clock_speed / 1000000;
 	uint16 id = msr >> 32;
-	int32 bus = freq / (id >> 8);
+	int32 bus = 0;
+	if (id >> 8)
+		freq / (id >> 8);
 
 	TRACE("est: Guessed bus clock (high) of %d MHz\n", int(bus));
 	if (!bus_speed_ok(bus)) {
 		// We may be running on the low frequency.
 		id = msr >> 48;
-		bus = freq / (id >> 8);
+		if (id >> 8)
+			bus = freq / (id >> 8);
 		TRACE("est: Guessed bus clock (low) of %d MHz\n", int(bus));
 		if (!bus_speed_ok(bus))
 			return B_ERROR;

commit 3cb6d0d08bb8c0b09da04619a5775061bc505251

Fixed error spotted by Axel, which I introduced in r35756. The bus value didn't get assigned anymore. Sorry about that!
git-svn-id: http://svn.haiku-os.org/haiku/haiku/trunk@35759 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/add-ons/kernel/drivers/power/enhanced_speedstep/frequency.cpp
@@ -146,7 +146,7 @@ est_msr_info(uint64 msr, freq_info** _frequencyInfos)
 	uint16 id = msr >> 32;
 	int32 bus = 0;
 	if (id >> 8)
-		freq / (id >> 8);
+		bus = freq / (id >> 8);
 
 	TRACE("est: Guessed bus clock (high) of %d MHz\n", int(bus));
 	if (!bus_speed_ok(bus)) {