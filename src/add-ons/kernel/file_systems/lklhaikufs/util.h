d4918b946e876d31ef00fbb4449a55f02e9f50ff
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-07-11 05:17:03 +0300

    lklhaikufs: refactor pathJoin
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

---
+++ src/add-ons/kernel/file_systems/lklhaikufs/util.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2010 Haiku Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *			Lucian Adrian Grijincu, lucian.grijincu@gmail.com
+ */
+
+
+extern char * pathJoin(const char * dirName, const char * fileName);
+
